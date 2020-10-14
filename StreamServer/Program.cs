6b250a5cbbf9d381da18d637b7b4c14c42759998
Ben Cargill <Benjamin.d.cargill@gmail.com>
2020-10-14 23:04:22 +0800

    Added Kestrel settings

--- StreamServer/Program.cs
+++ StreamServer/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System;
+using System.Net;
 
 namespace StreamServer
 {
@@ -12,6 +15,16 @@ namespace StreamServer
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureKestrel(options =>
+                {
+                    options.Limits.MaxConcurrentConnections = 5;
+                    options.Limits.MinRequestBodyDataRate = 
+                        new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                    options.Limits.MinResponseDataRate =
+                        new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
+                    options.Limits.Http2.MaxStreamsPerConnection = 2;
+                    options.Listen(IPAddress.Any, 53019);
+                })
                 .UseStartup<Startup>()
                 .Build();
     }
